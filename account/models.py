3f8a18c2e9e1a2056867527d2e38e45e7d258342
broskke <sapiramida123@gmail.com>
2023-07-10 21:25:24 +0600

    sad

---
+++ account/models.py
@@ -0,0 +1,36 @@
+# Create your models here.
+from uuid import uuid4
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField('email address', unique=True)
+    password = models.CharField(max_length=255)
+    activation_code = models.CharField(max_length=255, blank=True)
+    username = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField('first_name', max_length=150)
+    last_name = models.CharField('last_name', max_length=100)
+    avatar = models.ImageField(upload_to='media', blank=True, default='avatars/default_avatar.png')
+    is_active = models.BooleanField(
+        _('active'),
+        default=False,
+        help_text=_("Designates whether this user should be treated as active."
+                    "Unselect this instead of deleting accounts"))
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.email}'
+
+    def create_activation_code(self):
+        code = str(uuid4())
+        self.activation_code = code
